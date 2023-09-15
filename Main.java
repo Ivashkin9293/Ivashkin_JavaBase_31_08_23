e596b68cd419f73e5b0dd360ba415ab4f9e27e25
Ivashkin Serge <sergeivashkin1993@gmail.com>
2023-09-16 01:30:05 +0300

    commit Hello git

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Java Best");
         System.out.println("TEST TEST TEST!");
+        System.out.println("Hello Git");
     }
 
 }
